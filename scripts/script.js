cd3eda325bc52c4a3bcfeed9f6d00b7c7a5faf44
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-03-24 02:19:53 -0400

    Added button to reset grid, added createGrid, created hover function to simplify script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,38 @@
-for (i = 0;i < 256;i++) {
-    const container = document.querySelector("#container");
-    const gridSquare = document.createElement("div");
-    gridSquare.className = "square";
-    container.appendChild(gridSquare);
+function createGrid (desiredSquares) {
+    desiredArea = desiredSquares * desiredSquares;
+
+    for (i = 0;i < desiredArea;i++) {
+        const container = document.querySelector("#container");
+        const gridSquare = document.createElement("div");
+        gridSquare.className = "square";
+        container.appendChild(gridSquare);
+    };
+    document.documentElement.style.setProperty ('--number', desiredSquares);
 }
+
+function hover () {
+    const gridSquare = document.querySelectorAll(".square");
+    gridSquare.forEach(gridSquare => {
+        gridSquare.addEventListener ("mouseenter", () => {
+            gridSquare.style.backgroundColor = "teal";
+        });
+    });
+}
+
+let defaultSquares = 16;
+createGrid (defaultSquares);
+hover();
+
+const reset = document.querySelector("#resetButton");
+reset.addEventListener('click', () => {
+    let desiredSquares = prompt("Please enter the number of squares desired for each side. (maximum 100 squares)");
+    
+    if (desiredSquares > 100) {
+        alert("The number you entered was more than 100.");
+        return;
+    }
+
+    container.replaceChildren();
+    createGrid (desiredSquares);
+    hover ();
+});
